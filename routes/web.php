09b2ffebe2bc4c4dc55de4bfb1eec1bec6c80353
maruf25 <maruffiardy25@gmail.com>
2023-03-11 16:29:39 +0700

    fix route

--- routes/web.php
+++ routes/web.php
@@ -16,9 +16,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 
 Route::get('/dashboard', function () {
     return view('dashboard.index');
@@ -27,28 +27,7 @@ Route::get('/dashboard', function () {
 Route::get('/admin/coupons', AdminCouponsComponent::class)->name('admin.coupons');
 Route::get('/admin/coupons/add', AdminAddCouponsComponent::class)->name('admin.addcoupons');
 Route::get('/admin/coupons/edit/{idvoucher}', AdminEditCouponsComponent::class)->name('admin.editcoupons');
-<?php
 
-use Illuminate\Support\Facades\Route;
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
-
-// Route::get('/', function () {
-//     return view('welcome');
-// });
-
-// Route::get('/', function () {
-//     return view('Layouts.home');
-// });
 Route::get('/', function () {
     //return view('home.index');
     $data = [
@@ -57,21 +36,7 @@ Route::get('/', function () {
     return view('home.layouts.wrapper', $data);
 });
 
-Route::get('/film/search',[FilmController::class,'search']);
- <?php
-
-use Illuminate\Support\Facades\Route;
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
+Route::get('/film/search', [FilmController::class, 'search']);
 
 Route::get('/home', function () {
     return view('home.home');
